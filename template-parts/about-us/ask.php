148551e4d3b9260e1bfac4cd1bc623f255fa01da
jenea130 <jenea130@mail.ru>
2024-03-12 19:41:18 +0200

    upd: ask page about-us

--- template-parts/about-us/ask.php
+++ template-parts/about-us/ask.php
@@ -1,62 +1,26 @@
-<?php 
+<?php
 $ask = get_field('ask');
 $title = $aks['title'];
 $image = $aks['image'];
+$items = $ask['items'];
 ?>
 <section class="ask">
     <div class="ask__wrap">
         <div class="ask__content">
             <h2 class="ask__title"><?php echo $title; ?></h2>
             <div class="accordion">
-                <div class="accordion__item active">
-                    <div class="accordion__title">Aliquet lectus urna viverra in odio?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
+                <?php foreach ($items as $key => $item) : ?>
+                    <?php
+                    $title = $item['title'];
+                    $text = $item['text'];
+                    ?>
+                    <div class="accordion__item <?php echo $key === 0 ? 'active' : ''; ?>">
+                        <div class="accordion__title"><?php echo $title; ?></div>
+                        <div class="accordion__text">
+                            <?php echo $text; ?>
+                        </div>
                     </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">Orci commodo, viverra orci mollis ut euismod?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">Sagittis vitae facilisi rutrum amet mauris quisque vel convallis?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">In id dolor quis nunc, urna hendrerit pharetra?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">Orci commodo, viverra orci mollis ut euismod?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">Sagittis vitae facilisi rutrum amet mauris quisque vel convallis?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
-                <div class="accordion__item">
-                    <div class="accordion__title">In id dolor quis nunc, urna hendrerit pharetra?</div>
-                    <div class="accordion__text">
-                        Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum
-                        faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="ask__img">
