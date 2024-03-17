1d09a82ff0ceb5cd1566055db91ce25d9a4aa171
jenea130 <jenea130@mail.ru>
2024-03-18 01:52:33 +0200

    upd: single-case-studio

--- inc/taxonomy.php
+++ inc/taxonomy.php
@@ -7,6 +7,8 @@ add_action('init', 'create_taxonomy');
 function create_taxonomy()
 {
     $field = "Category Case Studies";
+    $field1 = "Tag Case Studies";
+
     register_taxonomy('category-case-studies', ['case-studio'], [
         'label' => '',
         'labels' => [
@@ -22,6 +24,32 @@ function create_taxonomy()
             'add_new_item' => 'Add New ' . $field,
             'new_item_name' => 'New',
             'menu_name' => $field,
+        ],
+        'description' => '',
+        'public' => true,
+        'hierarchical' => true,
+        'rewrite' => true,
+        'capabilities' => array(),
+        'meta_box_cb' => null,
+        'show_admin_column' => true,
+        'show_in_rest' => null,
+        'rest_base' => null,
+    ]);
+    register_taxonomy('tag-case-studies', ['case-studio'], [
+        'label' => '',
+        'labels' => [
+            'name' => $field1,
+            'singular_name' => $field1,
+            'search_items' => 'Search ' . $field1,
+            'all_items' => 'All ' . $field1,
+            'view_item ' => 'View ' . $field1,
+            'parent_item' => 'Parent ' . $field1,
+            'parent_item_colon' => 'Parent :.$field1',
+            'edit_item' => 'Edit ' . $field1,
+            'update_item' => 'Update ' . $field1,
+            'add_new_item' => 'Add New ' . $field1,
+            'new_item_name' => 'New',
+            'menu_name' => $field1,
         ],
         'description' => '',
         'public' => true,
