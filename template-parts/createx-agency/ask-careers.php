e6aa67c1f7adf41e92d869d61e6517b49b2d6712
jenea130 <jenea130@mail.ru>
2024-03-17 15:48:21 +0200

    feat: ask-careers

--- template-parts/createx-agency/ask-careers.php
+++ template-parts/createx-agency/ask-careers.php
@@ -1,124 +1,28 @@
+<?php
+$ask_careers = get_field('ask_careers');
+$button_text = $ask_careers['button_text'];
+$items = $ask_careers['items'];
+?>
 <section class="ask-careers">
   <div class="accordion">
-    <div class="accordion__item active">
-      <div class="accordion__wrap">
-        <div class="accordion__content">
-          <h3 class="accordion__title"><span>Digital Marketing Strategist</span><a class="btn btn--border" href="#">Apply now</a></h3>
-          <div class="accordion__text">
-            <p>Blandit quam eget fames nisl. Vel vestibulum dictum morbi risus. Mauris dictumst vitae convallis aliquam ullamcorper. Quis non non ac massa augue id scelerisque. Egestas eget cursus egestas habitant orci. In posuere tempus, vulputate est, diam arcu.</p>
-            <h3 class="accordion__subtitle">Requirements: </h3>
-            <ul class="accordion__list">
-              <li>Vitae luctus et arcu vitae eu tellus orci.</li>
-              <li>Vel morbi nunc ullamcorper nisl, quis varius ultricies blandit.</li>
-              <li>Purus cursus nascetur leo diam.</li>
-              <li>Nascetur id turpis facilisis faucibus vel, iaculis etiam.</li>
-            </ul>
-            <h3 class="accordion__subtitle">Responsibilities:</h3>
-            <ul class="accordion__list">
-              <li>Dui, lectus at dui magna vitae vitae egestas sed.</li>
-              <li>Vulputate placerat amet pulvinar lorem nisl.</li>
-              <li>Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.</li>
-              <li>Etiam duis lobortis in fames ultrices commodo nibh.</li>
-            </ul>
+    <?php foreach ($items as $key => $item) : ?>
+      <?php
+      $title = $item['title'];
+      $text = $item['text'];
+      ?>
+      <div class="accordion__item <?php echo $key === 0 ? 'active' : ''; ?>">
+        <div class="accordion__wrap">
+          <div class="accordion__content">
+            <h3 class="accordion__title">
+              <span><?php echo $title; ?></span>
+              <a class="btn btn--border" href="#"><?php echo $button_text; ?></a>
+            </h3>
+            <div class="accordion__text">
+              <?php echo $text; ?>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="accordion__item">
-      <div class="accordion__wrap">
-        <div class="accordion__content">
-          <h3 class="accordion__title"><span>Sales Executive</span><a class="btn btn--border" href="#">Apply now</a></h3>
-          <div class="accordion__text">
-            <p>Blandit quam eget fames nisl. Vel vestibulum dictum morbi risus. Mauris dictumst vitae convallis aliquam ullamcorper. Quis non non ac massa augue id scelerisque. Egestas eget cursus egestas habitant orci. In posuere tempus, vulputate est, diam arcu.</p>
-            <h3 class="accordion__subtitle">Requirements: </h3>
-            <ul class="accordion__list">
-              <li>Vitae luctus et arcu vitae eu tellus orci.</li>
-              <li>Vel morbi nunc ullamcorper nisl, quis varius ultricies blandit.</li>
-              <li>Purus cursus nascetur leo diam.</li>
-              <li>Nascetur id turpis facilisis faucibus vel, iaculis etiam.</li>
-            </ul>
-            <h3 class="accordion__subtitle">Responsibilities:</h3>
-            <ul class="accordion__list">
-              <li>Dui, lectus at dui magna vitae vitae egestas sed.</li>
-              <li>Vulputate placerat amet pulvinar lorem nisl.</li>
-              <li>Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.</li>
-              <li>Etiam duis lobortis in fames ultrices commodo nibh.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="accordion__item">
-      <div class="accordion__wrap">
-        <div class="accordion__content">
-          <h3 class="accordion__title"><span>Social Media Content Manager</span><a class="btn btn--border" href="#">Apply now</a></h3>
-          <div class="accordion__text">
-            <p>Blandit quam eget fames nisl. Vel vestibulum dictum morbi risus. Mauris dictumst vitae convallis aliquam ullamcorper. Quis non non ac massa augue id scelerisque. Egestas eget cursus egestas habitant orci. In posuere tempus, vulputate est, diam arcu.</p>
-            <h3 class="accordion__subtitle">Requirements: </h3>
-            <ul class="accordion__list">
-              <li>Vitae luctus et arcu vitae eu tellus orci.</li>
-              <li>Vel morbi nunc ullamcorper nisl, quis varius ultricies blandit.</li>
-              <li>Purus cursus nascetur leo diam.</li>
-              <li>Nascetur id turpis facilisis faucibus vel, iaculis etiam.</li>
-            </ul>
-            <h3 class="accordion__subtitle">Responsibilities:</h3>
-            <ul class="accordion__list">
-              <li>Dui, lectus at dui magna vitae vitae egestas sed.</li>
-              <li>Vulputate placerat amet pulvinar lorem nisl.</li>
-              <li>Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.</li>
-              <li>Etiam duis lobortis in fames ultrices commodo nibh.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="accordion__item">
-      <div class="accordion__wrap">
-        <div class="accordion__content">
-          <h3 class="accordion__title"><span> Digital Marketing Account Manager</span><a class="btn btn--border" href="#">Apply now</a></h3>
-          <div class="accordion__text">
-            <p>Blandit quam eget fames nisl. Vel vestibulum dictum morbi risus. Mauris dictumst vitae convallis aliquam ullamcorper. Quis non non ac massa augue id scelerisque. Egestas eget cursus egestas habitant orci. In posuere tempus, vulputate est, diam arcu.</p>
-            <h3 class="accordion__subtitle">Requirements: </h3>
-            <ul class="accordion__list">
-              <li>Vitae luctus et arcu vitae eu tellus orci.</li>
-              <li>Vel morbi nunc ullamcorper nisl, quis varius ultricies blandit.</li>
-              <li>Purus cursus nascetur leo diam.</li>
-              <li>Nascetur id turpis facilisis faucibus vel, iaculis etiam.</li>
-            </ul>
-            <h3 class="accordion__subtitle">Responsibilities:</h3>
-            <ul class="accordion__list">
-              <li>Dui, lectus at dui magna vitae vitae egestas sed.</li>
-              <li>Vulputate placerat amet pulvinar lorem nisl.</li>
-              <li>Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.</li>
-              <li>Etiam duis lobortis in fames ultrices commodo nibh.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="accordion__item">
-      <div class="accordion__wrap">
-        <div class="accordion__content">
-          <h3 class="accordion__title"><span>SEO Strategist</span><a class="btn btn--border" href="#">Apply now</a></h3>
-          <div class="accordion__text">
-            <p>Blandit quam eget fames nisl. Vel vestibulum dictum morbi risus. Mauris dictumst vitae convallis aliquam ullamcorper. Quis non non ac massa augue id scelerisque. Egestas eget cursus egestas habitant orci. In posuere tempus, vulputate est, diam arcu.</p>
-            <h3 class="accordion__subtitle">Requirements: </h3>
-            <ul class="accordion__list">
-              <li>Vitae luctus et arcu vitae eu tellus orci.</li>
-              <li>Vel morbi nunc ullamcorper nisl, quis varius ultricies blandit.</li>
-              <li>Purus cursus nascetur leo diam.</li>
-              <li>Nascetur id turpis facilisis faucibus vel, iaculis etiam.</li>
-            </ul>
-            <h3 class="accordion__subtitle">Responsibilities:</h3>
-            <ul class="accordion__list">
-              <li>Dui, lectus at dui magna vitae vitae egestas sed.</li>
-              <li>Vulputate placerat amet pulvinar lorem nisl.</li>
-              <li>Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.</li>
-              <li>Etiam duis lobortis in fames ultrices commodo nibh.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php endforeach; ?>
   </div>
 </section>
