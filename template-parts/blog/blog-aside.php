bc7fc9b86a0a1dc52baa7ccb726aaa39d2c8aeb0
jenea130 <jenea130@mail.ru>
2024-03-22 18:24:40 +0200

    feat: case-share

--- template-parts/blog/blog-aside.php
+++ template-parts/blog/blog-aside.php
@@ -1,24 +1,5 @@
-<?php
-$terms = get_tags();
-?>
 <div class="page-blog__aside">
   <?php echo get_template_part('template-parts/blog/blog-categories'); ?>
   <?php echo get_template_part('template-parts/blog/trending'); ?>
-  <div class="tags">
-    <h2 class="tags__title title title--small">Tags</h2>
-    <ul class="category-list">
-      <?php foreach ($terms as $term) : ?>
-        <?php
-        $name = $term->name;
-        $slug = $term->slug;
-        $tag_id = $term->term_id;
-        ?>
-        <li>
-          <a href="<?php echo get_the_permalink(27); ?>?tag_id=<?php echo $tag_id; ?>">
-            <?php echo "#" . $name; ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+  <?php echo get_template_part('template-parts/blog/tags'); ?>
 </div>
---
+++ template-parts/blog/tags.php
@@ -0,0 +1,20 @@
+<?php
+$terms = get_tags();
+?>
+<div class="tags">
+  <h2 class="tags__title title title--small">Tags</h2>
+  <ul class="category-list">
+    <?php foreach ($terms as $term) : ?>
+      <?php
+      $name = $term->name;
+      $slug = $term->slug;
+      $tag_id = $term->term_id;
+      ?>
+      <li>
+        <a href="<?php echo get_the_permalink(27); ?>?tag_id=<?php echo $tag_id; ?>">
+          <?php echo "#" . $name; ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
